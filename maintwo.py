ba976a5a0ec804b92a490d54700ff2611c9a0cf6
SeanMabli <smdrone1@gmail.com>
2022-04-09 14:58:50 -0400

    hyperpram tuning with wandb

--- maintwo.py
+++ maintwo.py
@@ -6,13 +6,16 @@ import wandb
 
 wandb.init(project="cnn-nonsquare")
 config = wandb.config
-config.filters = 11
-config.filtersize = 7
-config.convlr = 0.01966737225503975
-config.nnlr = 0.14892522221247584
-config.gen = 13442
-config.trainsize = 17336
-config.testsize = 1257
+config.filters = 4
+config.filtersize = 3
+config.convlr = 0.01
+config.nnlr = 0.1
+config.filterstwo = 4
+config.filtersizetwo = 3
+config.convlrtwo = 0.01
+config.gen = 10000
+config.trainsize = 5000
+config.testsize = 1000
 
 intrain, outtrain = extract_training_samples('digits')
 intrainreal = np.zeros((config.trainsize, 54, 40))
@@ -36,6 +39,8 @@ for i in range(config.testsize):
 
 model = ai.model((54, 40), 10, [
   ai.conv(inshape=(54, 40), filtershape=(config.filters, config.filtersize, config.filtersize), learningrate=config.convlr, activation=ai.relu()),
+  ai.pool(stride=(2, 2), filtershape=(2, 2), opperation='Max'),
+  ai.conv(inshape=(4, 26, 19), filtershape=(config.filterstwo, config.filtersizetwo, config.filtersizetwo), learningrate=config.convlrtwo, activation=ai.relu()),
   ai.pool(stride=(2, 2), filtershape=(2, 2), opperation='Max'),
   ai.nn(outshape=10, activation=ai.stablesoftmax(), learningrate=config.nnlr, weightsinit=(0, 0))
 ])
